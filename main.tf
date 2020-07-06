4858c09ca7a7238602d682a0aabdefeed82c345d
Eric Vallee <eric_vallee@webificservices.com>
2020-07-06 12:57:02 -0400

    Made ssh user customizable and refactored kubespray mention out of externally visible variable names

--- main.tf
+++ main.tf
@@ -9,7 +9,7 @@ resource "null_resource" "kubernetes_installation" {
   connection {
     host        = var.bastion_external_ip
     type        = "ssh"
-    user        = "ubuntu"
+    user        = var.bastion_user
     port        = var.bastion_port
     private_key = var.bastion_key_pair.private_key
   }
@@ -45,7 +45,7 @@ resource "null_resource" "kubernetes_installation" {
   
   provisioner "remote-exec" {
     inline = [
-      "ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_CONFIG=${var.cloud_init_sync_path}/ansible.cfg ansible-playbook --private-key=/home/ubuntu/.ssh/id_rsa --user ubuntu --inventory ${var.cloud_init_sync_path}/inventory --become --become-user=root ${var.cloud_init_sync_path}/sync.yml",
+      "ANSIBLE_HOST_KEY_CHECKING=False ANSIBLE_CONFIG=${var.cloud_init_sync_path}/ansible.cfg ansible-playbook --private-key=/home/${var.bastion_user}/.ssh/id_rsa --user ${var.k8_cluster_user} --inventory ${var.cloud_init_sync_path}/inventory --become --become-user=root ${var.cloud_init_sync_path}/sync.yml",
       "sudo rm -r ${var.cloud_init_sync_path}"
     ]
   }
@@ -53,64 +53,64 @@ resource "null_resource" "kubernetes_installation" {
   #Clone and prepup kubespray on a stable branch
   provisioner "remote-exec" {
     inline = [
-        "git clone https://github.com/kubernetes-sigs/kubespray.git ${var.kubespray_path}",
-        "mkdir -p ${var.kubespray_artifacts_path}",
-        "cd ${var.kubespray_path} && git checkout v2.13.2",
-        "cd ${var.kubespray_path} && sudo pip3 install -r requirements.txt",
-        "cd ${var.kubespray_path} && cp -rfp inventory/sample inventory/deployment",
+        "git clone https://github.com/kubernetes-sigs/kubespray.git ${var.provisioning_path}",
+        "mkdir -p ${var.artifacts_path}",
+        "cd ${var.provisioning_path} && git checkout v2.13.2",
+        "cd ${var.provisioning_path} && sudo pip3 install -r requirements.txt",
+        "cd ${var.provisioning_path} && cp -rfp inventory/sample inventory/deployment",
     ]
   }
 
   #Copy our custom configuration, inventory and run kubespray
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/etcd.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/etcd.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/etcd.yml"
   }
 
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/all/all.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/all/all.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/all/all.yml"
   }
 
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/all/containerd.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/all/containerd.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/all/containerd.yml"
   }
 
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/all/docker.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/all/docker.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/all/docker.yml"
   }
 
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/all/openstack.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/all/openstack.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/all/openstack.yml"
   }
 
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/k8s-cluster/addons.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/k8s-cluster/addons.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/k8s-cluster/addons.yml"
   }
 
   provisioner "file" {
     content     = templatefile(
       "${path.module}/kubespray/configurations/k8s-cluster/k8s-cluster.yml", 
       {
-        artifacts_dir = var.kubespray_artifacts_path
+        artifacts_dir = var.artifacts_path
         load_balancer_external_ip = var.load_balancer_external_ip
       }
     )
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/k8s-cluster/k8s-cluster.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/k8s-cluster/k8s-cluster.yml"
   }
 
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/k8s-cluster/k8s-net-calico.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/k8s-cluster/k8s-net-calico.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/k8s-cluster/k8s-net-calico.yml"
   }
 
   provisioner "file" {
     source      = "${path.module}/kubespray/configurations/k8s-cluster/k8s-net-flannel.yml"
-    destination = "${var.kubespray_path}/inventory/deployment/group_vars/k8s-cluster/k8s-net-flannel.yml"
+    destination = "${var.provisioning_path}/inventory/deployment/group_vars/k8s-cluster/k8s-net-flannel.yml"
   }
 
   provisioner "file" {
@@ -121,13 +121,13 @@ resource "null_resource" "kubernetes_installation" {
         worker_ips = var.worker_ips
       }
     )
-    destination = "${var.kubespray_path}/inventory/deployment/inventory"
+    destination = "${var.provisioning_path}/inventory/deployment/inventory"
   }
 
   provisioner "remote-exec" {
       inline = [
-          "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --private-key=/home/ubuntu/.ssh/id_rsa --user ubuntu --inventory ${var.kubespray_path}/inventory/deployment/inventory --become --become-user=root ${var.kubespray_path}/cluster.yml",
-          "sudo rm -r ${var.kubespray_path}"
+          "ANSIBLE_HOST_KEY_CHECKING=False ansible-playbook --private-key=/home/${var.bastion_user}/.ssh/id_rsa --user ${var.k8_cluster_user} --inventory ${var.provisioning_path}/inventory/deployment/inventory --become --become-user=root ${var.provisioning_path}/cluster.yml",
+          "sudo rm -r ${var.provisioning_path}"
       ]
   }
 }
